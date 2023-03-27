557b4a9926618f601cd102b0062240fe82eb25ff
MaTa31 <mickael-fernandez@live.fr>
2023-03-27 19:28:31 +0200

    Ajout des Routes CRUD + testing

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 import os
 
+from flask_sqlalchemy.session import Session
+from sqlalchemy import create_engine, func
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://FlaskApp:Studi@localhost:5432/mercadona'
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+
+username_db = "AdminMercadona"
+password_db = "Studi123"
+host_db = "localhost"
+port_db = "5432"
+database_name = "mercadona"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/mercadona"
+
+# = create_engine(f'postgresql://{username_db}:{password_db}@{host_db}:{port_db}/{database_name}')
+# session = Session(engine)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from models import Products
+from models import Products, Category
 
 
 @app.route('/')
@@ -19,5 +33,92 @@ def home():
     return render_template("index.html", products=products)
 
 
+@app.route('/panel')
+def panel():
+    categories = Category.query.all()
+    products = Products.query.all()
+    return render_template('panel.html', products=products, categories=categories)
+
+
+@app.route('/add_product', methods=['POST'])
+def add_product():
+    if request.method == 'POST':
+        name_product = request.form['name_product']
+        description = request.form['description']
+        category = request.form['category']
+        image = request.form['photo']
+        price = request.form['price']
+        date_promo = request.form['DateFinPromo']
+        promo = request.form['promoNumber']
+
+        if date_promo == '':
+            date_promo = None
+        if promo == '':
+            promo = None
+
+        print(name_product)
+
+        new_product = Products(name_product=name_product,
+                               description=description,
+                               category=category,
+                               # image=image,
+                               price=price,
+                               date_promo=date_promo,
+                               promo=promo)
+
+        db.session.add(new_product)
+        db.session.commit()
+
+        flash('Produit ajouté avec succée')
+        return redirect(url_for('panel'))
+
+
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def edit_product(id):
+    product_select = Products.query.get_or_404(id)
+    categories = Category.query.all()
+
+    if request.method == 'POST':
+        name_product = request.form['name_product']
+        description = request.form['description']
+        category = request.form['category']
+        image = request.form['photo']
+        price = request.form['price']
+        date_promo = request.form['DateFinPromo']
+        promo = request.form['promoNumber']
+
+        if date_promo == '':
+            date_promo = None
+        if promo == '':
+            promo = None
+
+        product_select.name_product = name_product
+        product_select.description = description
+        product_select.category = category
+        product_select.image = image
+        product_select.price = price
+        product_select.date_promo = date_promo
+        product_select.promo = promo
+
+        db.session.add(product_select)
+        db.session.commit()
+
+        flash('Produit mise à jour avec succée')
+        return redirect(url_for('panel'))
+
+    return render_template('edit.html', categories=categories, product=product_select)
+
+
+@app.route('/delete/<string:id>', methods=['POST', 'GET'])
+def delete_employee(id):
+    product_select = Products.query.get_or_404(id)
+    db.session.delete(product_select)
+    db.session.commit()
+    flash('Produit supprimer avec succée')
+    return redirect(url_for('panel'))
+
+
+# starting the app
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -4,22 +4,18 @@ from app import db
 class Products(db.Model):
     __tablename__ = 'products'
 
-    product_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name_product = db.Column(db.String(50))
     description = db.Column(db.String(500))
     category = db.Column(db.String(50))
-    image = db.Column(db.LargeBinary())
+    # image = db.Column(db.LargeBinary())
     price = db.Column(db.Float())
-    date_promo = db.Column(db.DateTime())
-    promo = db.Column(db.Integer())
+    date_promo = db.Column(db.DateTime(), nullable=True)
+    promo = db.Column(db.Float())
 
-    def __init__(self, product_id, name_product, description, category, image, price, date_promo, promo):
-        super().__init__()
-        self.id = product_id
-        self.category = category
-        self.name = name_product
-        self.promo = promo
-        self.description = description
-        self.image = image
-        self.price = price
-        self.date_promo = date_promo
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    category_id = db.Column(db.Integer, primary_key=True)
+    name_category = db.Column(db.String(50))
